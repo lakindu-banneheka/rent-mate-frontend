e6bb5892bc98d22fd3ec446b42e09438a590b319
lakindu-banneheka <lakindubannaheka@gmail.com>
2024-12-09 17:58:46 +0530

    fixed the bugs7.2 --build-errors

--- app/(admin_route)/page.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-    return (
-        <>
-            jahsdhnhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-        </>
-    )
-}
-
-export default Home;
