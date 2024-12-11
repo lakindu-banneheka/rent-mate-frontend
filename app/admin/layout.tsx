004bd20cea75328d80f8cadd579630996c34d981
lakindu-banneheka <64328828+lakindu-banneheka@users.noreply.github.com>
2024-12-11 23:52:55 +0530

    addded theme and added theme navigation for admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -9,6 +9,7 @@ import React, { ReactNode, useEffect, useState } from "react";
 import Loading from "../loading";
 import DynamicBreadcrumb from "@/components/dynamic-breadcrumb";
 import { UserRoles } from "@/types/userTypes";
+import { ThemeToggle } from "@/components/theme/theme-toggle";
 
 
 interface Props {
@@ -44,13 +45,16 @@ export default function AdminLayout ({ children }: Props) {
           userRole={UserRoles.Admin}
         />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4 fixed bg-sidebar w-full py-5"> {/* fixed - change the header fixed option */}
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4 bg-sidebar" />
-              <DynamicBreadcrumb />
-            </div>
-          </header>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 bg-background border-b">
+      <div className="flex items-center justify-between gap-2 px-4 w-full py-5">
+        <div className="flex items-center gap-2">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <DynamicBreadcrumb />
+        </div>
+        <ThemeToggle />
+      </div>
+    </header>
           <div className="flex flex-1 flex-col gap-0 p-0">
             <div className="pb-20 mx-5 bg-background" >
               {children}
