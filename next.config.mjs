8b32b7340caa27e1e26e9250734dca80bca5aa9a
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2024-12-26 21:47:12 +0530

    create browsepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['lh3.googleusercontent.com', 's3-ap-southeast-1.amazonaws.com', 's.gravatar.com', 'cdn.auth0.com'], 
-    },
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "s3-ap-southeast-1.amazonaws.com",
+      "s.gravatar.com",
+      "cdn.auth0.com",
+      "example.com",
+      "aerialworx.co.uk",
+    ],
+  },
 };
 
 export default nextConfig;
