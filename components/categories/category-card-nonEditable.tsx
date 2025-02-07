9c9cde2190c9c0be501fbb7af7b2d003ae4f6e2c
waruna <hashanrashmika2001@gmail.com>
2025-02-07 13:24:12 +0530

    create category details component

---
+++ components/categories/category-card-nonEditable.tsx
@@ -0,0 +1,236 @@
+'use client'
+
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+import { Category } from "@/types/categoryTypes"
+import { ImageUpload } from "../image/image-upload"
+import { AppDispatch, RootState } from "@/lib/store"
+import { useDispatch, useSelector } from "react-redux"
+import { createCategory, deleteCategory, updateCategory } from "@/lib/features/categorySlice"
+import { useToast } from "@/hooks/use-toast"
+
+const categorySchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  description: z.string().min(1, "Description is required"),
+  image: z.string().min(1, "Image is required"),
+})
+
+type FormData = z.infer<typeof categorySchema>
+
+interface CategoryFormProps {
+  category?: Category
+}
+
+export function CategoryForm({ category }: CategoryFormProps) {
+    const router = useRouter()
+    const { toast } = useToast();
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [s3ImgUrl, setS3ImgUrl] = useState<string | null>(null)
+    const dispatch: AppDispatch = useDispatch();
+    const error = useSelector((state: RootState) => state.category.error);
+
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isDirty },
+        setValue,
+        watch,
+    } = useForm<FormData>({
+        resolver: zodResolver(categorySchema),
+        defaultValues: category
+        ? {
+            name: category.name,
+            description: category.description,
+            image: category.imageUrl,
+        }
+        : {
+            image: "",
+        },
+    })
+
+    const image = watch("image");
+
+    useEffect(() => {
+        if(s3ImgUrl){
+            setValue("image", s3ImgUrl, { shouldValidate: true })
+        }
+    },[s3ImgUrl])
+
+    const onSubmit = async (data: FormData) => {
+        try {
+            setIsLoading(true)
+            if(s3ImgUrl){
+                if (category) {
+                    const categoryData: Category = {
+                        id: category ? category.id : "",
+                        name: data.name,
+                        description: data.description,
+                        imageUrl: s3ImgUrl,
+                        itemCount: category ? category.itemCount : 0,
+                        createdAt: category ? category.createdAt : new Date(),
+                        updatedAt: new Date()
+                    }
+                    await dispatch(updateCategory(categoryData));
+                } else {
+                    const categoryData = {
+                        name: data.name,
+                        description: data.description,
+                        imageUrl: s3ImgUrl,
+                        itemCount: 0
+        
+                    }
+                    await dispatch(createCategory(categoryData));
+                }
+            } else {
+                toast({
+                    variant: "destructive",
+                    title: "Error",
+                    description: "Image is required"
+                });
+            }
+
+            if (error) {
+                toast({
+                    variant: "destructive",
+                    title: `Error ${category ? 'updating' : 'creating'} category`,
+                    description: error
+                });
+            } else {
+                toast({
+                    variant: "default",
+                    title: "Success",
+                    description: `Category ${category ? 'updated' : 'created'} successfully`
+                });
+                router.push("/admin/categories")
+                router.refresh()
+            }
+            
+        } catch (err) {
+            console.error(err);
+            toast({
+                variant: "destructive",
+                title: "Unexpected error",
+                description: `Failed to ${category ? 'update' : 'create'} category`
+            });
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    const onRemove = () => {
+        setValue("image", "", { shouldValidate: true })
+        // axios.delete("/api/delete", { data: { url: watch('image')} })
+        // .then(() => {
+        //     toast({
+        //         variant: "default",
+        //         title: "Success",
+        //         description: "Image deleted successfully"
+        //     });
+        //     setValue("image", "", { shouldValidate: true })
+        // })
+        // .catch((error) => {
+        //     toast({
+        //         variant: "destructive",
+        //         title: "Error",
+        //         description: "Failed to delete image"
+        //     });
+        //     console.error("Delete error:", error);
+        // });
+    }
+
+    const handleDeleteCategory = async (id: string) => {
+        try {
+            setIsLoading(true);
+            const result = await dispatch(deleteCategory(id));
+            
+            if (error) {
+                toast({
+                    variant: "destructive",
+                    title: "Error deleting category",
+                    description: result.payload as string
+                });
+            } else {
+                toast({
+                    title: "Success",
+                    description: "Category deleted successfully"
+                });
+                router.push("/admin/categories")
+                router.refresh()
+            }
+            
+        } catch (err) {
+            console.error(err);
+            toast({
+                variant: "destructive",
+                title: "Unexpected error",
+                description: "Failed to delete category"
+            });
+        } finally {
+            setIsLoading(false)
+        }
+    };
+
+    useEffect(() => {
+        const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+          if (isDirty) {
+            event.preventDefault();
+            event.returnValue = "You have unsaved changes. Are you sure you want to leave?";
+          }
+        };
+    
+        window.addEventListener("beforeunload", handleBeforeUnload);
+    
+        return () => {
+          window.removeEventListener("beforeunload", handleBeforeUnload);
+        };
+    }, [isDirty]);
+
+    return (
+        <Card>
+            <CardContent className="p-6">
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                    <div className="space-y-2">
+                        <Label htmlFor="name">Name</Label>
+                        <Input disabled id="name" {...register("name")} />
+                        {errors.name && (
+                            <p className="text-sm text-destructive">{errors.name.message}</p>
+                        )}
+                    </div>
+                    <div className="space-y-2">
+                        <Label htmlFor="description">Description</Label>
+                        <Textarea disabled id="description" {...register("description")} />
+                        {errors.description && (
+                            <p className="text-sm text-destructive">
+                                {errors.description.message}
+                            </p>
+                        )}
+                    </div>
+                    <div className="space-y-2">
+                        <ImageUpload
+                            value={image}
+                            disabled
+                            onChange={(url) => setS3ImgUrl(url)}
+                            onRemove={onRemove}
+                            s3ImgUrl={s3ImgUrl}
+                            // maxImages={1}
+                        />
+                        {errors.image && (
+                            <p className="text-sm text-destructive">{errors.image.message}</p>
+                        )}
+                    </div>
+                </form>
+            </CardContent>
+        </Card>
+    )
+}
+
